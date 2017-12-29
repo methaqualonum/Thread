eb0cc602c65fabee8e1b5506c555987068e07db6
methaqualonum <echelon896@gmail.com>
2017-12-29 14:27:50 +0300

    Добавлены константы с путями до файлов.

--- src/ru/svs/thread/copyFiles/Start.java
+++ src/ru/svs/thread/copyFiles/Start.java
@@ -1,10 +1,13 @@
 package ru.svs.thread.copyFiles;
 
 public class Start {
+    private static final String FILE_TXT = "/home/methaqualonum/IdeaProjects/Thread/src/ru/svs/thread/copyFiles/file.txt";
+    private static final String FIRST_OUTPUT_TXT = "/home/methaqualonum/IdeaProjects/Thread/src/ru/svs/thread/copyFiles/firstOutput.txt";
+    private static final String SECOND_OUTPUT_TXT = "/home/methaqualonum/IdeaProjects/Thread/src/ru/svs/thread/copyFiles/secondOutput.txt";
+
     public static void main(String[] args) throws InterruptedException {
         long before = System.currentTimeMillis();
-        Copy one = new Copy("/home/methaqualonum/IdeaProjects/Thread/src/ru/svs/thread/copyFiles/file.txt",
-                "/home/methaqualonum/IdeaProjects/Thread/src/ru/svs/thread/copyFiles/firstOutput.txt");
+        Copy one = new Copy(FILE_TXT, FIRST_OUTPUT_TXT);
         one.start();
 
         if(one.isAlive()){
@@ -13,8 +16,7 @@ public class Start {
         System.out.println("Выполнение первого: "+(System.currentTimeMillis() - before) + " милисекунд");
 
         long beforeOne = System.currentTimeMillis();
-        Copy two = new Copy("/home/methaqualonum/IdeaProjects/Thread/src/ru/svs/thread/copyFiles/file.txt",
-                "/home/methaqualonum/IdeaProjects/Thread/src/ru/svs/thread/copyFiles/secondOutput.txt");
+        Copy two = new Copy(FILE_TXT, SECOND_OUTPUT_TXT);
         two.start();
         if(two.isAlive()){
             two.join();
